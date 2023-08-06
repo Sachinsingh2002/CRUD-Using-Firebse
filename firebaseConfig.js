643db46edc17f88d0f2086e4713eadcdc4401d69
Sachin Singh <108183252+Sachinsingh2002@users.noreply.github.com>
2023-08-06 20:31:31 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+  
+  apiKey: "AIzaSyAZ07tBgKf_nDrIPAj7HvksHjdqMNYdyqI",
+  authDomain: "first-firebase-project-3e8d8.firebaseapp.com",
+  databaseURL: "https://first-firebase-project-3e8d8-default-rtdb.firebaseio.com",
+  projectId: "first-firebase-project-3e8d8",
+  storageBucket: "first-firebase-project-3e8d8.appspot.com",
+  messagingSenderId: "552596767355",
+  appId: "1:552596767355:web:e954965eddf607effa58de",
+
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
